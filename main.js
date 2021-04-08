0ebc204c2b40ce59ad97090cdbe64955a69b9f7f
johnfr14 <jonathan.alyra@gmail.com>
2021-04-08 17:29:52 +0200

    finish

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ while (player.length < 1 || player.length > 20) {
 
 // le presentateur nous pose une question (readlineSync) et on donne une reponse qui sera stocké dans une variable
 console.log(`\n\nBien Bien Bien ${chalk.yellow(player)}, tu as maintenant 3 portes devant toi avec une fefe derriere l\'une d'entre elles !
-Choissis 1 porte, MAIS ATTENTION !!!! choissis la bonne ;)`)
+Choissis 1 porte, ${chalk.bgRed('MAIS ATTENTION')} !!!! choissis la bonne ;)`)
 
 let playerChoice = readLine.keyInSelect(tab[0], `Votre Choix : `)
 
@@ -48,28 +48,29 @@ const revealedDoor = (tab, playerChoice) => {
 } 
 
 // Le presentateur nous montre une porte qui cache une chèvre (évidemment pas la notre si elle en contenait une)
-console.log(`Très bien tu as choisi la ${tab[0][playerChoice]}. Maintenant laisse moi te dire que dans la ${tab[0][revealedDoor(tab, playerChoice)]} il y avait une ${tab[1][revealedDoor(tab, playerChoice)]}`)
+console.log(`Très bien tu as choisi la ${chalk.yellow(tab[0][playerChoice])}. Maintenant laisse moi te dire que dans la ${chalk.red(tab[0][revealedDoor(tab, playerChoice)])} il y avait une ${chalk.red(tab[1][revealedDoor(tab, playerChoice)])}`)
 
 // derniere chance pour le joueur
 let playerChange = readLine.keyInYN('\nVoulez changer votre choix ? derniere chance ! : ')
 
+// a t'il gagné ? a t'il perdu ?
 switch (playerChange) {
   case true :
     if (tab[1][playerChoice] === 'ferrari') {
-      console.log('\nOHHHHHH quelle dommage ! vous aviez la bonne porte au début mais vous avez changé pour une belle chèvrette x)\n')
+      console.log(chalk.red('\nOHHHHHH quelle dommage ! vous aviez la bonne porte au début mais vous avez changé pour une belle chèvrette x)\n'))
     } else {
-      console.log('\nomg full chance gg vous gagnez la fefe !!!!!\n')
+      console.log(chalk.green('\nomg full chance gg vous gagnez la fefe !!!!!\n'))
     }
     break
   case false :
     if (tab[1][playerChoice] === 'ferrari') {
-      console.log('\nOui vous êtes un homme sage et n\'aviez pas été tenté par la tention du stress d\'un retournement de situation dus à l\'accumulation d\émotion incertaine, bref vous avez la fefe\n')
+      console.log(chalk.green('\nOui vous êtes un homme sage et n\'aviez pas été tenté par la tention du stress d\'un retournement de situation dus à l\'accumulation d\émotion incertaine, bref vous avez la fefe\n'))
     } else {
-      console.log('\nVoici votre nouvelle femme "Madame chevre"\n')
+      console.log(chalk.red('\nVoici votre nouvelle femme "Madame chevre"\n'))
     }
     break
   default :
-    console.log('\nFelicitation vous avez ni*** mon code !!!!\n')
+    console.log(chalk.bgBlackBright('\nFelicitation vous avez ni*** mon code !!!!\n'))
 }
 
 console.log(chalk.bgBlack(`      Le tableau !       \n ${tab[0]} \n    ↓      ↓      ↓      \n ${tab[1]}   \n`))
